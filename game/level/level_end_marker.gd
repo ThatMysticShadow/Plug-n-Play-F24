da3e38ca86bc4bd1f9e1476b045e99d5c6ad6269
Dolphcode <bastikulit@gmail.com>
2024-10-20 15:12:48 -0400

    Added player base and level end marker

---
+++ game/level/level_end_marker.gd
@@ -0,0 +1,16 @@
+extends Area2D
+## As the name implies, this class represents what the player has to touch in order to
+## complete the level. Your level must have a level end marker in order for the player
+## to be able to beat your level and choose another level. DO NOT modify this code.
+class_name LevelEndMarker
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	body_entered.connect(_on_area_entered)
+	area_entered.connect(_on_area_entered)
+
+
+func _on_area_entered(other):
+	if other is Player:
+		print("You have cleared this level")
