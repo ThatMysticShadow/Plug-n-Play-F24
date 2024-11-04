f6d32fa36fd743f3fb85d78781a8dbb5b985bba8
Dolphcode <bastikulit@gmail.com>
2024-11-04 00:12:34 -0500

    Update tileset and finish creating legally distinct enemy

--- base/game/level/hazards/enemies/basic enemies/walking_enemy.gd
+++ base/game/level/hazards/enemies/basic enemies/walking_enemy.gd
@@ -5,6 +5,8 @@ class_name WalkingEnemy
 
 @export_category("Movement Config")
 @export var horizontal_speed: float = 50
+@export var max_fall_speed: float = 200
+@export var gravity: float = 1000
 
 func _ready():
 	super._ready()
@@ -12,4 +14,12 @@ func _ready():
 
 func _physics_process(delta):
 	velocity = Vector2(horizontal_speed, 0)
+	
+	# Not a necessary optimization but only apply gravity if the player's fall speed is not at its maximum
+	if velocity.y < max_fall_speed:
+		velocity.y += delta * gravity
+		
+		# Clamp the player's velocity to this lower bound
+		if velocity.y > max_fall_speed:
+			velocity.y = max_fall_speed
 	move_and_slide()
---
+++ base/game/level/hazards/enemies/enemy_one_way_hitbox.gd
@@ -0,0 +1,34 @@
+@tool
+extends EnemyHitbox
+
+class_name EnemyOneWayHitbox
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	area_entered.connect(_on_hitbox_entered)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+## This is the default hitbox detection behavior. It makes the assumption that the opposing hurtbox
+## is a component/child of the object responsible for dealing damage to the enemy. It checks if that
+## object has a get_source_damage() method and applies that damage to the enemy accordingly.
+func _on_hitbox_entered(other: Area2D):
+	# Ignore player hitboxes, player hitboxes should not cause damage'
+	if other is PlayerHitbox:
+		return
+	
+	var direction = global_position - other.global_position
+	direction = direction.normalized()
+	if direction.y <= 0:
+		return
+	
+	var source = other.get_parent()
+	if source.has_method("get_source_damage"):
+		enemy_reference.damage(source.get_source_damage())
+	else:
+		print_rich("[color=yellow]<WARNING: A potential damage source \"" + str(source.name) + 
+		"\" that does not have a get_source_damage() method has entered the " + str(enemy_reference.name) + " hitbox>")
