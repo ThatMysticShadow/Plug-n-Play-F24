0b3daf2a44f9a678d7deb65a3f7d1029c93ae7ad
Justin Segovia <thatmysticshadow@gmail.com>
2024-11-21 18:53:30 -0500

    animations

--- dev/level/new_funcs.gd
+++ dev/level/new_funcs.gd
@@ -8,12 +8,23 @@ extends Player
 
 @export var enemy_hit_sound: AudioStream
 
+@onready var anim_player2 = $sprite_animation
+
+var iframes : int
+
+var isInv : bool = false
+
+func _ready():
+	current_health = starting_health
+	footstep_time = footstep_max_time
+
 ## Overwrites damage() and removes sound_player from get_source_damage() and moves it to check_death()
 func damage(amount: float) -> void:
-	current_health -= amount
-	current_health = clampf(current_health, 0, max_health)
-	check_death()
-
+	if(!isInv): 
+		current_health -= amount
+		current_health = clampf(current_health, 0, max_health)
+		check_death()
+	
 ## Overwrites get_source_damage() and removes sound_player from get_source_damage() and moves it to check_death()
 func get_source_damage() -> float:
 	sound_player.play_sound(enemy_hit_sound, global_position)
@@ -31,8 +42,40 @@ func check_death() -> void:
 		print_rich("[color=red]Player has died")
 		level_loader.reload_level()
 
+## Overwrites update_animation to also check if the player is falling and update the animation
+func update_animation() -> void:
+	if (!is_on_floor() && velocity.y < 0):
+		anim_player2.play("jump")
+	elif (velocity.y > 0):
+		anim_player2.play("fall")
+	else:
+		if abs(velocity.x) < STOP_VELOCITY_THRSHOLD:
+			anim_player2.play("idle")
+		else:
+			anim_player2.play("walk")
+			
+	sprite.flip_h = true if sign(velocity.x) < 0 else (false if sign(velocity.x) > 0  else sprite.flip_h)
+
 ## The function is called when the player loses their shield (1 hp) and allows the player to 
 ## bounce off any non-enemy hazards
 func shield_bounce() -> void:
 	velocity.y = -jump_strength
-	
+	#velocity.x = -(velocity.x-15) 
+	%Shield.visible = false
+	isInv = true
+	%damage.start()
+
+func player_flicker() -> void:
+	$Sprite2D.visible = false
+	await get_tree().create_timer(0.2).timeout
+	$Sprite2D.visible = true
+	print("die")
+
+func _on_damage_timeout() -> void:
+	isInv = false
+
+func _process(delta: float) -> void:
+	if(isInv):
+		await player_flicker()
+		
+		
